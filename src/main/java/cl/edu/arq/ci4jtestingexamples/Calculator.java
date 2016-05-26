b1147fc2b1c998cb23c062715879b7585880ca57
Eduardo León García <edo.leon.g@gmail.com>
2016-05-26 00:07:46 -0300

    Adds more basic examples

--- src/main/java/cl/edu/arq/ci4jtestingexamples/Calculator.java
+++ src/main/java/cl/edu/arq/ci4jtestingexamples/Calculator.java
@@ -2,8 +2,12 @@ package cl.edu.arq.ci4jtestingexamples;
 
 public class Calculator {
 
-	public static double toSquare(double base) {
+	public double toSquare(double base) {
 		return Math.pow(base, 2);
+	}
+
+	public double sqrt(double a) {
+		return Math.sqrt(a);
 	}
 
 }
--- src/test/java/cl/edu/arq/ci4jtestingexamples/CalculatorTest.java
+++ src/test/java/cl/edu/arq/ci4jtestingexamples/CalculatorTest.java
@@ -6,10 +6,36 @@ import cl.edu.arq.ci4jtestingexamples.Calculator;
 
 public class CalculatorTest {
 	
+	private Calculator calculator = new Calculator();
+	
 	@Test
 	public void testSquarePow() {
-		double result = Calculator.toSquare(3);
-		Assert.assertEquals(9d, result, 0d);
+		double result = calculator.toSquare(7);
+		Assert.assertEquals(49D, result, 0D);
+	}
+	
+	@Test
+	public void testInfiniteSquareRoot() {
+		double result = calculator.sqrt(Double.POSITIVE_INFINITY);
+		Assert.assertEquals(Double.POSITIVE_INFINITY, result, 0D);
+	}
+	
+	@Test
+	public void testNegativeSquareRoot() {
+		double result = calculator.sqrt(-1);
+		Assert.assertEquals(Double.NaN, result, 0D);
+	}
+	
+	@Test
+	public void testZeroSquareRoot() {
+		double result = calculator.sqrt(0D);
+		Assert.assertEquals(-0D, result, 0D);
+	}
+	
+	@Test
+	public void testPerfectSquareRoot() {
+		double result = calculator.sqrt(64D);
+		Assert.assertEquals(8D, result, 0D);
 	}
 
 }
